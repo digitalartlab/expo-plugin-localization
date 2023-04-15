c077811e696ab0f3e765e3289c6ad41e3daf5bfa
Thijmen de Valk <thijmen@madebythijmen.nl>
2023-04-15 22:44:37 +0200

    build: stop tracking build folder

--- build/withLocalizableProject.d.ts
+++
@@ -1,7 +0,0 @@
-import { ConfigPlugin } from "expo/config-plugins";
-/**
- * Adds a Localizable.strings file reference to the Xcode project for each region. This is necessary for Xcode to recognize the various languages.
- */
-export declare const withLocalizableProject: ConfigPlugin<{
-    knownRegions: string[];
-}>;
--- build/withLocalizableResources.d.ts
+++
@@ -1,8 +0,0 @@
-import { ConfigPlugin } from "expo/config-plugins";
-/**
- * Adds the actual Localizable.strings files to the iOS project folder. These files are empty and are only used to satisfy Xcode.
- * This is a dangerous mod because it writes to the file system.
- */
-export declare const withLocalizableResources: ConfigPlugin<{
-    knownRegions: string[];
-}>;
