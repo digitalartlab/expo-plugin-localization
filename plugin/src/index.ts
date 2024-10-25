7d6c4a04a095ef8305883818532b8b574f6660c5
Thijmen de Valk <10213180+ThijmenDeValk@users.noreply.github.com>
2024-10-25 11:29:52 +0200

    feat: simplify iOS implementation using CFBundleLocalizations (#23 and #20)

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -9,7 +9,6 @@ import { withAndroidLocalizableGradle } from "./withAndroidLocalizableGradle";
 import { withAndroidLocalizableManifest } from "./withAndroidLocalizableManifest";
 import { withAndroidLocalizableResources } from "./withAndroidLocalizableResources";
 import { withIosLocalizableProject } from "./withIosLocalizableProject";
-import { withIosLocalizableResources } from "./withIosLocalizableResources";
 
 const withNativeLocaleSwitching: ConfigPlugin<{ locales?: string[] }> = (
   config,
@@ -34,7 +33,6 @@ const withNativeLocaleSwitching: ConfigPlugin<{ locales?: string[] }> = (
   });
 
   config = withIosLocalizableProject(config, { locales });
-  config = withIosLocalizableResources(config, { locales });
   config = withAndroidLocalizableGradle(config, { locales });
   config = withAndroidLocalizableManifest(config);
   config = withAndroidLocalizableResources(config, { locales });
--- plugin/src/withIosLocalizableProject.ts
+++ plugin/src/withIosLocalizableProject.ts
@@ -1,4 +1,4 @@
-import { ConfigPlugin, withXcodeProject } from "expo/config-plugins";
+import { ConfigPlugin, withInfoPlist } from "expo/config-plugins";
 
 /**
  * Adds a Localizable.strings file reference to the Xcode project for each locale. This is necessary for Xcode to recognize the various languages.
@@ -6,29 +6,11 @@ import { ConfigPlugin, withXcodeProject } from "expo/config-plugins";
 export const withIosLocalizableProject: ConfigPlugin<{
   locales: string[];
 }> = (config, { locales }) => {
-  return withXcodeProject(config, async (config) => {
-    const xcodeProject = config.modResults;
-    locales.forEach((locale) => {
-      // Add the locale to the project
-      // Deduplication is handled by the function
-      xcodeProject.addKnownRegion(locale);
-    });
-
-    xcodeProject.addPbxGroup("Resources", "Resources");
-
-    const localizationVariantGp = xcodeProject.addLocalizationVariantGroup(
-      "Localizable.strings",
-    );
-    const localizationVariantGpKey = localizationVariantGp.fileRef;
-
-    locales.forEach((locale) => {
-      // Create a file reference for each locale
-      xcodeProject.addResourceFile(
-        `Resources/${locale}.lproj/Localizable.strings`,
-        { variantGroup: true },
-        localizationVariantGpKey,
-      );
-    });
+  return withInfoPlist(config, (config) => {
+    config.modResults["LOCALES_SUPPORTED"] = locales.join(",");
+    config.modResults["CFBundleAllowMixedLocalizations"] = true;
+    config.modResults["CFBundleLocalizations"] = locales;
+    config.modResults["CFBundleDevelopmentRegion"] = "en";
     return config;
   });
 };
